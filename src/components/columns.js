cf05ed32c61e1e2d8576a6462d146996b30fe0db
Henry Tochukwu Uzor <h.uzor17@gmail.com>
2024-01-19 18:58:36 +0100

    feat: columns of the react table defined

---
+++ src/components/columns.js
@@ -0,0 +1,30 @@
+export const COLUMNS = [
+  {
+    Header: 'Id',
+    accessor: 'id'
+  },
+  {
+    Header: 'First Name',
+    accessor: 'first_name'
+  },
+  {
+    Header: 'Last Name',
+    accessor: 'last_name'
+  },
+  {
+    Header: 'Gender',
+    accessor: 'gender'
+  },
+  {
+    Header: 'Date of Birth',
+    accessor: 'date_of_birth'
+  },
+  {
+    Header: 'Country',
+    accessor: 'country'
+  },
+  {
+    Header: 'Phone',
+    accessor: 'phone'
+  }
+]

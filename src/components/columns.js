8667b966bc59a177a2a852ada8875ad6ba67c634
Henry Tochukwu Uzor <h.uzor17@gmail.com>
2024-01-20 00:37:13 +0100

    feat: footers defined in columns file

--- src/components/columns.js
+++ src/components/columns.js
@@ -1,30 +1,37 @@
 export const COLUMNS = [
   {
     Header: 'Id',
+    Footer: 'Id',
     accessor: 'id'
   },
   {
     Header: 'First Name',
+    Footer: 'First Name',
     accessor: 'first_name'
   },
   {
     Header: 'Last Name',
+    Footer: 'Last Name',
     accessor: 'last_name'
   },
   {
     Header: 'Gender',
+    Footer: 'Gender',
     accessor: 'gender'
   },
   {
     Header: 'Date of Birth',
+    Footer: 'Date of Birth',
     accessor: 'date_of_birth'
   },
   {
     Header: 'Country',
+    Footer: 'Country',
     accessor: 'country'
   },
   {
     Header: 'Phone',
+    Footer: 'Phone',
     accessor: 'phone'
   }
 ]

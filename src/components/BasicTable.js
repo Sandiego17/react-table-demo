61380a407fbbb00e3cae3a7cf762f4b2ca5c8cab
Henry Tochukwu Uzor <h.uzor17@gmail.com>
2024-01-21 13:12:31 +0100

    feat: GlobalFilter function imported and used in table

--- src/components/BasicTable.js
+++ src/components/BasicTable.js
@@ -1,8 +1,9 @@
-import { useSortBy, useTable } from 'react-table'
+import { useGlobalFilter, useSortBy, useTable } from 'react-table'
 import { COLUMNS } from './columns'
 import MOCK_DATA from './MOCK_DATA.json'
 import { useMemo } from 'react'
 import './BasicTable.css'
+import { GlobalFilter } from './GlobalFilter'
 
 export const BasicTable = () => {
   const columns = useMemo(() => COLUMNS, [])
@@ -11,7 +12,7 @@ export const BasicTable = () => {
   const tableInstance = useTable({
     columns,
     data
-  }, useSortBy)
+  }, useGlobalFilter, useSortBy)
 
   const {
     getTableProps,
@@ -19,46 +20,58 @@ export const BasicTable = () => {
     headerGroups,
     footerGroups,
     rows,
-    prepareRow
+    prepareRow,
+    state,
+    setGlobalFilter
   } = tableInstance
 
+  const { globalFilter } = state
+
   return (
-    <table {...getTableProps()}>
-      <thead>
-        {headerGroups.map((headerGroup) => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column) => (
-              <th {...column.getHeaderProps(column.getSortByToggleProps())}>
-                {column.render('Header')}
-                <span>
-                  {column.isSorted ? (column.isSortedDesc ? ' 🔽' : ' 🔼') : ''}
-                </span>
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row) => {
-          prepareRow(row)
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map((cell) => {
-                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-              })}
+    <>
+      <div>
+        <h1>React Table</h1>
+      </div>
+
+      <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter} />
+
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.render('Header')}
+                  <span>
+                    {column.isSorted ? (column.isSortedDesc ? ' 🔽' : ' 🔼') : ''}
+                  </span>
+                </th>
+              ))}
             </tr>
-          )
-        })}
-      </tbody>
-      <tfoot>
-        {footerGroups.map((footerGroup) => (
-          <tr {...footerGroup.getFooterGroupProps()}>
-            {footerGroup.headers.map((column) => (
-              <th {...column.getFooterProps()}>{column.render('Footer')}</th>
-            ))}
-          </tr>
-        ))}
-      </tfoot>
-    </table>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row)
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+        <tfoot>
+          {footerGroups.map((footerGroup) => (
+            <tr {...footerGroup.getFooterGroupProps()}>
+              {footerGroup.headers.map((column) => (
+                <th {...column.getFooterProps()}>{column.render('Footer')}</th>
+              ))}
+            </tr>
+          ))}
+        </tfoot>
+      </table>
+    </>
   )
 }

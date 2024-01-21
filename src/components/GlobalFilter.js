868e2bee64c4e3f5a06b9244e905b73e5449417a
Henry Tochukwu Uzor <h.uzor17@gmail.com>
2024-01-21 13:11:51 +0100

    feat: GlobalFilter function defined and exported

---
+++ src/components/GlobalFilter.js
@@ -0,0 +1,8 @@
+export const GlobalFilter = ({ filter, setFilter }) => {
+  return (
+    <span>
+      Search:{' '}
+      <input value={filter || ''} onChange={(e) => setFilter(e.target.value)} />
+    </span>
+  )
+}

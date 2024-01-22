46bb686c60fbbf35060967a00db34a5db2372653
Henry Tochukwu Uzor <h.uzor17@gmail.com>
2024-01-22 18:28:05 +0100

    feat: demonstrating gotoPage

--- src/components/PaginationTable.js
+++ src/components/PaginationTable.js
@@ -68,6 +68,19 @@ export const PaginationTable = () => {
           <span>Page {pageIndex + 1} of {pageOptions.length}</span>
         </h3>
 
+        <p>
+          Go to page: {' '}
+          <input
+            type='number'
+            defaultValue={pageIndex + 1}
+            onChange={(e) => {
+              const pageNumber = e.target.value ? Number(e.target.value) - 1 : 0
+              gotoPage(pageNumber)
+            }}
+            style={{ width: '50px' }}
+          />
+        </p>
+
         <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>{'<<'}</button>
         <button onClick={() => previousPage()} disabled={!canPreviousPage}>Previous</button>
         <button onClick={() => nextPage()} disabled={!canNextPage}>Next</button>

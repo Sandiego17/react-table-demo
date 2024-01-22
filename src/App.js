691a66a9ec9a7a3c13c2414a94beb4c73c12d5c1
Henry Tochukwu Uzor <h.uzor17@gmail.com>
2024-01-22 18:12:43 +0100

    feat: pagination - next and previous

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { BasicTable } from './components/BasicTable';
+import { PaginationTable } from './components/PaginationTable';
 
 const App = () => {
   return (
     <div className="App">
-      <BasicTable />
+      {/* <BasicTable /> */}
+      <PaginationTable />
     </div>
   );
 }
---
+++ src/components/PaginationTable.js
@@ -0,0 +1,74 @@
+import { usePagination, useTable } from 'react-table'
+import { COLUMNS } from './columns'
+import MOCK_DATA from './MOCK_DATA.json'
+import { useMemo } from 'react'
+import './BasicTable.css'
+
+export const PaginationTable = () => {
+  const columns = useMemo(() => COLUMNS, [])
+  const data = useMemo(() => MOCK_DATA, [])
+
+  const tableInstance = useTable({
+    columns,
+    data
+  }, usePagination)
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    page,
+    nextPage,
+    canNextPage,
+    previousPage,
+    canPreviousPage,
+    pageOptions,
+    state,
+    prepareRow
+  } = tableInstance
+
+  const { pageIndex } = state
+
+  return (
+    <>
+      <div>
+        <h1>React Table</h1>
+      </div>
+
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>
+                  {column.render('Header')}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map((row) => {
+            prepareRow(row)
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+
+      <div>
+        <h3>
+          <span>Page {pageIndex + 1} of {pageOptions.length}</span>
+        </h3>
+
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>Previous</button>
+        <button onClick={() => nextPage()} disabled={!canNextPage}>Next</button>
+      </div>
+    </>
+  )
+}
